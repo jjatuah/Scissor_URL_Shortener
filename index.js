17a70026ebd961e94c947e3b731f9854cbf619a0
unknown <jjatuah@gmail.com>
2023-06-18 14:27:28 +0100

    setup mongodb connection

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express()
+const mongoose = require("mongoose");
 
 const PORT = 5000
 
-app.set('view engine', 'ejs')
+mongoose.connect("mongodb://localhost:27017/Scissor", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
 
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index')
