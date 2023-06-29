71e44a3012f2769af54606510c15503caecdb16a
unknown <jjatuah@gmail.com>
2023-06-29 22:27:20 +0100

    Added cors

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const Database = require("./dbConnect")
 const urlRoute = require("./routes/url.route")
+const cors = require("cors");
 require('dotenv').config();
 
 const app = express()
 
 
 const PORT = process.env.PORT || 5000
+
+
+const corsOrigin ={
+  origin:'*', //or whatever port your frontend is using
+  credentials:true,            
+  optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
+
+
+
 
 // connect to database
 Database.connect();
